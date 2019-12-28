3cebb602f99f340f6f0e1b1f7d970cb0457dcb70
yhl234 <200392342@student.georgianc.on.ca>
2019-12-29 02:24:53 +0800

    created main nav

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<nav>
+		<Ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Works</a></li>
+			<li><a href="#">Resume</a></li>
+			<li><a href="#">Contact</a></li>
+		</Ul>
+	</nav>
+</template>
+<script>
+export default {
+	name: "Home"
+};
+</script>
+<style scoped>
+nav {
+	background: rgb(47, 46, 53);
+}
+ul {
+	display: flex;
+	list-style: none;
+	justify-content: flex-end;
+}
+li {
+	padding: 1rem;
+}
+a {
+	color: #ffffff;
+}
+</style>
