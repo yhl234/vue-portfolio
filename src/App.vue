f249e7adf57b7098b4f7b4f91be2b85fcb62edd1
yhl234 <200392342@student.georgianc.on.ca>
2019-12-30 01:47:29 +0800

    colour variable

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-	<div id="app">
-		<Home />
-	</div>
+  <div id="app">
+    <Home />
+  </div>
 </template>
 
 <script>
 import Home from "./components/Home.vue";
 
 export default {
-	name: "app",
-	components: {
-		Home
-	}
+  name: "app",
+  components: {
+    Home
+  }
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+:root {
+  --color-intrative: #ca3e47;
+  --color-dark-one: #313131;
+  --color-dark-two: #414141;
+  --color-dark-three: #525252;
+}
 body {
-	margin: 0;
-	padding: 0;
+  background: var(--color-dark-one);
+  color: var(--color-dark-three);
+  margin: 0;
+  padding: 0;
 }
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #abafb3;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,60 @@
 <template>
-	<nav>
-		<Ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">Works</a></li>
-			<li><a href="#">Resume</a></li>
-			<li><a href="#">Contact</a></li>
-		</Ul>
-	</nav>
+  <div>
+    <nav>
+      <Ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Works</a>
+        </li>
+        <li>
+          <a href="#">Resume</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </Ul>
+    </nav>
+    <div class="home-grid">
+      <h1>Louis Lee</h1>
+      <div class="social-links">
+        <i class="fab fa-linkedin fa-2x"></i>
+        <i class="fab fa-github-square fa-2x"></i>
+        <i class="fas fa-envelope-square fa-2x"></i>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-	name: "Home"
+  name: "Home"
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 nav {
-	background: rgb(47, 46, 53);
+  background: transparent;
 }
 ul {
-	display: flex;
-	list-style: none;
-	justify-content: flex-end;
+  display: flex;
+  list-style: none;
+  justify-content: flex-end;
 }
 li {
-	padding: 1rem;
+  padding: 1rem;
 }
 a {
-	color: #ffffff;
+  color: #ffffff;
+}
+.home-grid {
+  display: grid;
+}
+
+.social-links {
+  display: flex;
+  justify-content: flex-start;
+  i {
+    margin: 0.5rem;
+  }
 }
 </style>
