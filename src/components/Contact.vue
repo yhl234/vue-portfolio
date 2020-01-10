dad75fc29fe5fd7f3c2cd16e429a440933a0a380
yhl234 <200392342@student.georgianc.on.ca>
2020-01-10 18:10:44 -0500

    contact form works

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,71 +1,78 @@
 <template>
-	<div class="bottom">
-		<form @submit.prevent="sendEmail">
-			<label>Name</label>
-			<input type="text" name="user_name" />
-			<label>Email</label>
-			<input type="email" name="user_email" />
-			<label>Message</label>
-			<textarea name="message"></textarea>
-			<button type="submit" value="Send">Submit</button>
-		</form>
-	</div>
+  <div class="bottom" v-cloak>
+    <form class="contact-form" @submit.prevent="sendEmail">
+      <label>Name</label>
+      <input type="text" name="user_name" v-model="name" />
+      <label>Email</label>
+      <input type="email" name="user_email" v-model="email" />
+      <label>Message</label>
+      <textarea name="message" v-model="message"></textarea>
+      <button type="submit" value="Send">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import emailjs from "emailjs-com";
 
 export default {
-	name: "Contact",
-	methods: {
-		sendEmail: e => {
-			emailjs.sendForm(
-				"YOUR_SERVICE_ID",
-				"YOUR_TEMPLATE_ID",
-				e.target,
-				"YOUR_USER_ID"
-			);
-			// .then(
-			//   result => {
-			//     console.log("SUCCESS!", response.status, response.text);
-			//   },
-			//   error => {
-			//     console.log("FAILED...", error);
-			//   }
-			// );
-		}
-	}
+  name: "Contact",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      message: ""
+    };
+  },
+  methods: {
+    sendEmail() {
+      emailjs
+        .send(
+          "gmail",
+          "template_wBaZWgJK",
+          { name: this.name, email: this.email, message: this.message },
+          "user_nA6P8cuhfuXYF41H0AfBJ"
+        )
+        .then(
+          response => {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          error => {
+            console.log("FAILED...", error);
+          }
+        );
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 .bottom {
-	display: flex;
-	justify-content: center;
-	text-align: left;
+  display: flex;
+  justify-content: center;
+  text-align: left;
 }
 form {
-	// height: 60%;
-	width: 50%;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 }
 input,
 textarea {
-	margin-bottom: 3rem;
-	border: 0;
-	padding: 0.5rem;
-	border-radius: 0.2rem;
-	background: var(--color-text);
+  margin-bottom: 3rem;
+  border: 0;
+  padding: 0.5rem;
+  border-radius: 0.2rem;
+  background: var(--color-text);
 }
 label {
-	margin-bottom: 1rem;
+  margin-bottom: 1rem;
 }
 button {
-	padding: 0.5rem;
-	width: 30%;
-	border: 0.5px solid #fff;
-	border-collapse: collapse;
-	background: transparent;
+  padding: 0.5rem;
+  width: 30%;
+  border: 0.5px solid #fff;
+  border-collapse: collapse;
+  background: transparent;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,59 +1,60 @@
 <template>
-	<div class="home">
-		<!-- Photo by Patrick Tomasso on Unsplash -->
-		<div class="top center-center">
-			<h1>Louis Lee</h1>
-		</div>
-		<template v-if="this.menu === 'home'">
-			<Works />
-		</template>
-		<template v-if="this.menu === 'contact'">
-			<Contact />
-		</template>
-	</div>
+  <div class="home">
+    <!-- Photo by Patrick Tomasso on Unsplash -->
+    <div class="top center-center">
+      <h1>Louis Lee</h1>
+    </div>
+    <template v-if="this.menu === 'home'">
+      <Works />
+    </template>
+    <template v-if="this.menu === 'contact'">
+      <Contact />
+    </template>
+  </div>
 </template>
 <script>
 import Works from "./Works";
 import Contact from "./Contact";
 export default {
-	name: "Home",
-	props: ["menu"],
-	components: {
-		Works,
-		Contact
-	}
+  name: "Home",
+  props: ["menu"],
+  components: {
+    Works,
+    Contact
+  }
 };
 </script>
 <style lang="scss" scoped>
 h1 {
-	font-family: "Fredericka the Great", cursive;
+  font-family: "Fredericka the Great", cursive;
 }
 .home {
-	display: grid;
-	position: relative;
-	grid-template-areas: "top" "bottom";
-	grid-template-rows: 300px auto;
+  display: grid;
+  position: relative;
+  grid-template-areas: "top" "bottom";
+  grid-template-rows: 300px auto;
 }
 .home::after {
-	content: "";
-	background-image: url("../assets/img/bg.jpg");
-	// Photo by Patrick Tomasso on Unsplash
-	background-size: cover;
-	background-position: center;
-	background-repeat: no-repeat;
-	opacity: 0.1;
-	top: 0;
-	left: 0;
-	bottom: 0;
-	right: 0;
-	position: absolute;
-	z-index: -1;
+  content: "";
+  background-image: url("../assets/img/bg.jpg");
+  // Photo by Patrick Tomasso on Unsplash
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  opacity: 0.1;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  z-index: -1;
 }
 .top {
-	grid-area: top;
+  grid-area: top;
 }
 .bottom {
-	grid-area: bottom;
-	padding: 0 3rem;
+  grid-area: bottom;
+  padding: 0 3rem;
+  transition: all 5000s ease-in-out;
 }
 </style>
