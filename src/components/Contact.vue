17ce7c0ad53156d5883bbb27358afa220a6f160a
yhl234 <200392342@student.georgianc.on.ca>
2020-01-10 18:41:44 -0500

    installed VueRecaptcha

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,78 +1,83 @@
+/* eslint-disable no-console */
 <template>
-  <div class="bottom" v-cloak>
-    <form class="contact-form" @submit.prevent="sendEmail">
-      <label>Name</label>
-      <input type="text" name="user_name" v-model="name" />
-      <label>Email</label>
-      <input type="email" name="user_email" v-model="email" />
-      <label>Message</label>
-      <textarea name="message" v-model="message"></textarea>
-      <button type="submit" value="Send">Submit</button>
-    </form>
-  </div>
+	<div class="bottom" v-cloak>
+		<form id="contact" class="contact-form" @submit.prevent="sendEmail">
+			<label>Name</label>
+			<input type="text" name="user_name" v-model="name" />
+			<label>Email</label>
+			<input type="email" name="user_email" v-model="email" />
+			<label>Message</label>
+			<textarea name="message" v-model="message"></textarea>
+			<vue-recaptcha
+				sitekey="6LePUM4UAAAAAHb4c65hKC0yK9N3moIyerHsWNjn"
+			></vue-recaptcha>
+			<button type="submit" value="Send">Submit</button>
+		</form>
+	</div>
 </template>
 
 <script>
 import emailjs from "emailjs-com";
-
+import VueRecaptcha from "vue-recaptcha";
 export default {
-  name: "Contact",
-  data: function() {
-    return {
-      name: "",
-      email: "",
-      message: ""
-    };
-  },
-  methods: {
-    sendEmail() {
-      emailjs
-        .send(
-          "gmail",
-          "template_wBaZWgJK",
-          { name: this.name, email: this.email, message: this.message },
-          "user_nA6P8cuhfuXYF41H0AfBJ"
-        )
-        .then(
-          response => {
-            console.log("SUCCESS!", response.status, response.text);
-          },
-          error => {
-            console.log("FAILED...", error);
-          }
-        );
-    }
-  }
+	name: "Contact",
+	components: { VueRecaptcha },
+	data: function() {
+		return {
+			name: "",
+			email: "",
+			message: ""
+		};
+	},
+	methods: {
+		sendEmail() {
+			emailjs
+				.send(
+					"gmail",
+					"template_wBaZWgJK",
+					{ name: this.name, email: this.email, message: this.message },
+					"user_nA6P8cuhfuXYF41H0AfBJ"
+				)
+				.then(
+					response => {
+						console.log("SUCCESS!", response.status, response.text);
+					},
+					error => {
+						console.log("FAILED...", error);
+					}
+				);
+		}
+	}
 };
 </script>
 <style lang="scss" scoped>
 .bottom {
-  display: flex;
-  justify-content: center;
-  text-align: left;
+	display: flex;
+	justify-content: center;
+	text-align: left;
 }
-form {
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+#contact {
+	width: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
 }
-input,
+input:not(#google),
 textarea {
-  margin-bottom: 3rem;
-  border: 0;
-  padding: 0.5rem;
-  border-radius: 0.2rem;
-  background: var(--color-text);
+	margin-bottom: 3rem;
+	border: 0;
+	padding: 0.5rem;
+	border-radius: 0.2rem;
+	background: var(--color-text);
 }
 label {
-  margin-bottom: 1rem;
+	margin-bottom: 1rem;
 }
 button {
-  padding: 0.5rem;
-  width: 30%;
-  border: 0.5px solid #fff;
-  border-collapse: collapse;
-  background: transparent;
+	padding: 0.5rem;
+	width: 30%;
+	border: 0.5px solid #fff;
+	border-collapse: collapse;
+	background: transparent;
 }
 </style>
