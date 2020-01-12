ddd46f150d21e5157eb2c4f2bde94425a85a92d5
yhl234 <200392342@student.georgianc.on.ca>
2020-01-12 16:51:49 -0500

    validateForm

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,83 +1,110 @@
 /* eslint-disable no-console */
 <template>
-	<div class="bottom" v-cloak>
-		<form id="contact" class="contact-form" @submit.prevent="sendEmail">
-			<label>Name</label>
-			<input type="text" name="user_name" v-model="name" />
-			<label>Email</label>
-			<input type="email" name="user_email" v-model="email" />
-			<label>Message</label>
-			<textarea name="message" v-model="message"></textarea>
-			<vue-recaptcha
-				sitekey="6LePUM4UAAAAAHb4c65hKC0yK9N3moIyerHsWNjn"
-			></vue-recaptcha>
-			<button type="submit" value="Send">Submit</button>
-		</form>
-	</div>
+  <div class="bottom" v-cloak>
+    <form id="contact" class="contact-form" @submit.prevent="validateForm">
+      <div class="required" v-for=" (i,index) in this.validate" :key="index">{{i}}</div>
+      <label>Name</label>
+      <input type="text" name="user_name" v-model="name" />
+      <label>Email</label>
+      <input type="email" name="user_email" v-model="email" />
+      <label>Message</label>
+      <textarea name="message" v-model="message"></textarea>
+      <vue-recaptcha sitekey="6LePUM4UAAAAAHb4c65hKC0yK9N3moIyerHsWNjn"></vue-recaptcha>
+      <button type="submit" value="Send">Submit</button>
+      <div>{{this.status}}</div>
+    </form>
+  </div>
 </template>
 
 <script>
 import emailjs from "emailjs-com";
 import VueRecaptcha from "vue-recaptcha";
 export default {
-	name: "Contact",
-	components: { VueRecaptcha },
-	data: function() {
-		return {
-			name: "",
-			email: "",
-			message: ""
-		};
-	},
-	methods: {
-		sendEmail() {
-			emailjs
-				.send(
-					"gmail",
-					"template_wBaZWgJK",
-					{ name: this.name, email: this.email, message: this.message },
-					"user_nA6P8cuhfuXYF41H0AfBJ"
-				)
-				.then(
-					response => {
-						console.log("SUCCESS!", response.status, response.text);
-					},
-					error => {
-						console.log("FAILED...", error);
-					}
-				);
-		}
-	}
+  name: "Contact",
+  components: { VueRecaptcha },
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+      status: "",
+      validate: []
+    };
+  },
+  methods: {
+    validateForm() {
+      this.validate = [];
+      this.status = "";
+      if (this.name === "") {
+        this.validate.push("Name is required");
+      }
+      if (this.email === "") {
+        this.validate.push("Email is required");
+      }
+      if (this.message === "") {
+        this.validate.push("Message is required");
+      }
+      if (this.validate.length === 0) {
+        this.sendEmail();
+      }
+    },
+    sendEmail() {
+      emailjs
+        .send(
+          "gmail",
+          "template_wBaZWgJK",
+          { name: this.name, email: this.email, message: this.message },
+          "user_nA6P8cuhfuXYF41H0AfBJ"
+        )
+        .then(
+          response => {
+            this.status = "Send! Thank you for your time";
+            this.name = "";
+            this.email = "";
+            this.message = "";
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          error => {
+            this.status = "FAILED... Please try again later";
+            console.log("FAILED...", error);
+          }
+        );
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
+.required {
+  color: var(--color-intrative);
+  margin-bottom: 0.5rem;
+}
 .bottom {
-	display: flex;
-	justify-content: center;
-	text-align: left;
+  display: flex;
+  justify-content: center;
+  text-align: left;
 }
 #contact {
-	width: 50%;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 }
 input:not(#google),
 textarea {
-	margin-bottom: 3rem;
-	border: 0;
-	padding: 0.5rem;
-	border-radius: 0.2rem;
-	background: var(--color-text);
+  margin-bottom: 3rem;
+  border: 0;
+  padding: 0.5rem;
+  border-radius: 0.2rem;
+  background: var(--color-text);
 }
 label {
-	margin-bottom: 1rem;
+  margin-bottom: 1rem;
 }
 button {
-	padding: 0.5rem;
-	width: 30%;
-	border: 0.5px solid #fff;
-	border-collapse: collapse;
-	background: transparent;
+  padding: 0.5rem;
+  width: 30%;
+  border: 0.5px solid #fff;
+  border-collapse: collapse;
+  background: transparent;
 }
 </style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,70 +1,73 @@
 <template>
-  <div class="main-nav">
-    <nav>
-      <Ul>
-        <li>
-          <a href="#" data-menu="home" @click="emitMenu">Home</a>
-        </li>
-        <li>
-          <a href="#" data-menu="about" @click="emitMenu">About</a>
-        </li>
-        <li>
-          <a href="#" data-menu="contact" @click="emitMenu">Contact</a>
-        </li>
-      </Ul>
-    </nav>
-    <div class="social-links">
-      <a href="#">
-        <i class="fab fa-linkedin fa-2x"></i>
-      </a>
-      <a href="#">
-        <i class="fab fa-github-square fa-2x"></i>
-      </a>
-      <a href="#">
-        <i class="fas fa-envelope-square fa-2x"></i>
-      </a>
-    </div>
-  </div>
+	<div class="main-nav">
+		<nav>
+			<Ul>
+				<li>
+					<a href="#" data-menu="home" @click="emitMenu">Home</a>
+				</li>
+				<li>
+					<a href="#" data-menu="about" @click="emitMenu">About</a>
+				</li>
+				<li>
+					<a href="#" data-menu="contact" @click="emitMenu">Contact</a>
+				</li>
+			</Ul>
+		</nav>
+		<div class="social-links">
+			<a
+				href="https://www.linkedin.com/in/louis-lee-a85159129"
+				title="Louis Lee"
+			>
+				<i class="fab fa-linkedin fa-2x"></i>
+			</a>
+			<a href="https://github.com/yhl234" title="yhl234">
+				<i class="fab fa-github-square fa-2x"></i>
+			</a>
+			<a href="mailto:leejepn@gmail.com" title="leejepn@gmail.com">
+				<i class="fas fa-envelope-square fa-2x"></i>
+			</a>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  name: "MainNav",
-  methods: {
-    emitMenu(e) {
-      this.$emit("menuClicked", e.target.dataset.menu);
-    }
-  }
+	name: "MainNav",
+	methods: {
+		emitMenu(e) {
+			this.$emit("menuClicked", e.target.dataset.menu);
+		}
+	}
 };
 </script>
 <style lang="scss" scoped>
 .main-nav {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
 }
 nav {
-  ul {
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    list-style: none;
-    justify-content: flex-end;
-    li {
-      padding: 1rem;
-      a {
-        display: block;
-        padding: 0.3rem;
-        text-decoration: none;
-      }
-    }
-  }
+	ul {
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+		list-style: none;
+		justify-content: flex-end;
+		li {
+			padding: 1rem;
+			a {
+				display: block;
+				padding: 0.3rem;
+				text-decoration: none;
+			}
+		}
+	}
 }
 .social-links {
-  display: flex;
-  justify-content: space-evenly;
-  a:hover {
-    background: transparent;
-    color: var(--color-intrative);
-  }
+	display: flex;
+	justify-content: space-evenly;
+	a:hover {
+		background: transparent;
+		color: var(--color-intrative);
+	}
 }
 </style>
